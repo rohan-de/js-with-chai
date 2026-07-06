ad5660d1e376be76f77ec0af95fe2b4b78562082
Rohan <149697636+rohan-de@users.noreply.github.com>
2026-07-06 15:26:12 +0000

    config

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3); // code readability should be high
+
+console.log("rohan");
+
+//Data types
+
+let name = "Rohan" //string
+let age = 18 //age
+let isLoggedIn = false //boollean
+let state; //undefiend
+
+//Premetive DataTypes
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+
+console.log(typeof "Rohan") //string
+console.log(typeof "18") //number
+console.log(typeof false) //boolean
