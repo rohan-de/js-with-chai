c16b3d6e4fffaf6701fb44189a1acd3d25d9bba9
Rohan <149697636+rohan-de@users.noreply.github.com>
2026-07-06 16:13:45 +0000

    config

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "rohan"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//If i convert "33" => they easily convert 33
+//If "33abc" => not converted and they show NaN (Not a Number , but type is number)
+//If i use value "true" convert => 1; False value convert => 0;
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+//console.log(typeof booleanIsLoggedIn);
+
+//if i type 1 => true; 0=> false;
+//if i use "" (the value is) => false 
+//if i use some value in string "rohan" => true;
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
